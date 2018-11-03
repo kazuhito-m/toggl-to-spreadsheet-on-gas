e9753b092008c040b63cdecf0e369d4117e30e98
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-11-03 10:48:03 +0900

    テスト実装。DateRaneのテスト。

---
+++ tests/domain/date/DateRange.spec.ts
@@ -0,0 +1,13 @@
+import TextableDate from '../../../src/domain/date/TextableDate';
+import DateRange from '../../../src/domain/date/DateRange';
+describe('DateRange のテスト.', () => {
+  it('「その月の月初日と月末日」を取得、返すことが出来る。', () => {
+    const mockNow = new Date(2012, 1, 14, 0, 0, 0); // うるう年の２月
+    DateRange.generator = { now: () => mockNow };
+
+    const actual = DateRange.nowMonth();
+
+    expect('2012-02-01').toEqual(actual.start.format());
+    expect('2012-02-29').toEqual(actual.end.format());
+  });
+});
