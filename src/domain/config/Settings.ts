26f29eed89354086c5d6438ca45a4bd3b36f8bf1
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-11-03 12:43:42 +0900

    実装。TogglからAPIでデータを引っ張ってくるとこまで実装。

--- src/domain/config/Settings.ts
+++ src/domain/config/Settings.ts
@@ -1,3 +1,5 @@
 export default interface Settings {
   togglToken: string;
+  projectId: string;
+  workspaceId: string;
 }
--- src/infrastracture/datasource/toggl/TogglRepository.ts
+++ src/infrastracture/datasource/toggl/TogglRepository.ts
@@ -1,11 +1,16 @@
 import TogglRepository from 'src/domain/toggl/TogglRepository';
 import Achievements from '../../../domain/toggl/Achievements';
 import Settings from '../../../domain/config/Settings';
+import DateRange from '../../../domain/date/DateRange';
+import TogglApi from './TogglApi';
 
 export default class TogglDatasource implements TogglRepository {
+  private readonly api = new TogglApi();
+
   public loadTwoMonth(settings: Settings): Achievements {
-    // TODO 実装。
-    Logger.log('TogglDatasource.loadTwoMonth() 未実装');
+    const nowMonth = DateRange.nowMonth();
+    const lastMonth = nowMonth.previousMonth();
+
     return new Achievements([]);
   }
 }
