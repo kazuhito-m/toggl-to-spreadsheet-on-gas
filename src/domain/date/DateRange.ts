12a501771e91fcc0652c7a570355c691a34a7f38
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-11-03 10:46:21 +0900

    実装。DateRangeクラス。

--- src/domain/date/DateRange.ts
+++ src/domain/date/DateRange.ts
@@ -1,20 +1,26 @@
 import TextableDate from './TextableDate';
+import NowGenerator from './NowGenerator';
 export default class DateRange {
+  public static generator: NowGenerator = { now: (): Date => new Date() };
+
   public static nowMonth(): DateRange {
-    const now = DateRange.now();
+    const now = DateRange.generator.now();
     const year = now.getFullYear();
     const month = now.getMonth();
+
+    console.log('year:' + year);
+    console.log('month:' + month);
+
     const startDay = new Date(year, month, 1, 0, 0, 0);
-    const endDay = new Date(year, month + 1, 1, 0, 0, 0);
+    const endDay = new Date(year, month + 1, 0, 0, 0, 0);
+
+    console.log('endDay:' + endDay);
+
     return new DateRange(new TextableDate(startDay), new TextableDate(endDay));
   }
 
-  private static now(): Date {
-    return new Date();
-  }
-
   constructor(
-    private readonly from: TextableDate,
-    private readonly to: TextableDate,
+    public readonly start: TextableDate,
+    public readonly end: TextableDate,
   ) {}
 }
