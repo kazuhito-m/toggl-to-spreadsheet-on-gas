317728aa1984457a0e2fb57714b9f15fabce8982
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-11-03 09:57:45 +0900

    実装。「日付の範囲」もつクラスを実装。

--- src/domain/date/DateRange.ts
+++ src/domain/date/DateRange.ts
@@ -1,3 +1,20 @@
+import TextableDate from './TextableDate';
 export default class DateRange {
-  constructor(private readonly from: Date, private readonly to: Date) {}
+  public static nowMonth(): DateRange {
+    const now = DateRange.now();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+    const startDay = new Date(year, month, 1, 0, 0, 0);
+    const endDay = new Date(year, month + 1, 1, 0, 0, 0);
+    return new DateRange(new TextableDate(startDay), new TextableDate(endDay));
+  }
+
+  private static now(): Date {
+    return new Date();
+  }
+
+  constructor(
+    private readonly from: TextableDate,
+    private readonly to: TextableDate,
+  ) {}
 }
