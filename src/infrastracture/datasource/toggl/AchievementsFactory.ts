d474971e6fd04650da5824b5d1440b8a46b220e3
kazuhito-m <kazuhito.sumpic@gmail.com>
2018-11-03 12:58:52 +0900

    実装。Detasourceから通信してアイテムを取ってくるところの「構造」のみを実装。

---
+++ src/infrastracture/datasource/toggl/AchievementsFactory.ts
@@ -0,0 +1,8 @@
+import DetailsResponse from './response/DetailsResponse';
+import Achievements from '../../../domain/toggl/Achievements';
+
+export default class AchievementsFactory {
+  public create(responses: DetailsResponse[]): Achievements {
+    return [];
+  }
+}
--- src/infrastracture/datasource/toggl/TogglRepository.ts
+++ src/infrastracture/datasource/toggl/TogglRepository.ts
@@ -3,14 +3,18 @@ import Achievements from '../../../domain/toggl/Achievements';
 import Settings from '../../../domain/config/Settings';
 import DateRange from '../../../domain/date/DateRange';
 import TogglApi from './TogglApi';
+import AchievementsFactory from './AchievementsFactory';
 
 export default class TogglDatasource implements TogglRepository {
   private readonly api = new TogglApi();
-
+  private readonly factory = new AchievementsFactory();
   public loadTwoMonth(settings: Settings): Achievements {
     const nowMonth = DateRange.nowMonth();
     const lastMonth = nowMonth.previousMonth();
 
-    return new Achievements([]);
+    const nowMonthResponse = this.api.getRequestDetails(nowMonth, settings);
+    const lastMonthResponse = this.api.getRequestDetails(lastMonth, settings);
+
+    return this.factory.create([nowMonthResponse, lastMonthResponse]);
   }
 }
